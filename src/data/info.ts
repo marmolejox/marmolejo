09bb4d088f2e59f9ff1f2715f378345eca336e22
marmolejox <marmolejx@gmail.com>
2024-05-20 12:24:15 -0600

    initial commit

---
+++ src/data/info.ts
@@ -0,0 +1,169 @@
+export const info = {
+  baseUrl: "https://google.com",
+  name: "Eduardo Marmolejo",
+  jobDescription: "Desarrollador",
+  about: `Ingeniero en Sistemas con 2 años de experiencia en desarrollo web, móvil y de escritorio. Conocimientos sólidos en C++, Java, Python, Angular y React. Experiencia en análisis de sistemas, diseño UX y metodologías ágiles. Apasionado por crear soluciones robustas y de excelente diseño. Objetivo de aprender nuevas tecnologías y emprender mi propia desarrolladora de software. Profesional comprometido, enfocado en resultados de alta calidad.
+  `,
+
+  experience: [
+    {
+      name: "Criasts Development",
+      job: "Desarrollador",
+      location: "Aguascalientes, Aguascalientes",
+      startDate: "Junio 2023",
+      endDate: "Diciembre 2023",
+      description: [
+        "✓ Desarrollé páginas web institucionales, blogs y gubernamentales utilizando Angular y WordPress, brindando soluciones escalables y de alto rendimiento.",
+        "✓ Implementé y administré bases de datos NoSQL eficientes con MongoDB para almacenar grandes volúmenes de datos extraídos mediante web scraping, como notas y noticias, utilizados posteriormente para desarrollar APIs internas.",
+        "✓ Lideré tareas de mantenimiento y monitoreo de servidores web, analizando el rendimiento e investigando incidencias para garantizar una operación continua y estable.",
+        "✓ Optimicé el rendimiento front-end mediante la migración de Bootstrap a Tailwind CSS, mejorando los tiempos de carga y la experiencia de usuario.",
+        "✓ Colaboré en un equipo de 6 desarrolladores, aplicando metodologías ágiles para cumplir con los plazos y requisitos del proyecto.",
+      ],
+    },
+
+    {
+      name: "Elefante Printshop",
+      job: "Encargado General",
+      location: "Nochistlán, Zacatecas",
+      startDate: "Enero 2017",
+      endDate: "Agosto 2021",
+      description: [
+        "✓ Optimicé los procesos de servicio implementando redes locales que permitían impresión remota desde equipos de clientes.",
+        "✓ Desarrollé sitios web personalizados en WordPress con funcionalidades para invitaciones digitales.",
+        "✓ Coordiné el control de inventario mediante software personalizado y automaticé procesos con scripts Bash.",
+        "✓ Realicé mantenimiento preventivo y correctivo periódico al equipo de cómputo y maquinaria.",
+        "✓ Supervisé y administré económicamente las operaciones diarias liderando a 4 empleados ayudantes.",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Universidad Autónoma de Aguascalientes (UAA)",
+      location: "Aguascalientes, Aguascalientes",
+      startDate: "2018",
+      endDate: "2023",
+      job: "Licenciatura en Ingeniería en Sistemas Computacionales",
+      description: [
+        "• Coordinador del XVII Congreso del Centro de Ciencias Básicas, a cargo de logística y organización.",
+        "• 4° lugar en la XVLI Muestra Emprendedora con 'VIG-IA', sistema de monitoreo con IA para estacionamientos.",
+        "• Tesis 'Timecapsule': aplicación web de aprendizaje interactivo implementada en escuela primaria."
+      ],
+    },
+    {
+      name: "Centros de Bachillerato Tecnológico Industrial y de Servicios #104 (CBTis)",
+      location: "Sombrerete, Zacatecas",
+      startDate: "2015",
+      endDate: "2018",
+      job: "Programación",
+      description: [
+        "• Desarrollé mis primeras aplicaciones web utilizando HTML, CSS y JavaScript, incluyendo bloques de notas, listas de pendientes y un sistema de registro.",
+        "• Como proyecto final en equipo, creé un videojuego 'Memorama' para dispositivos móviles utilizando Java",
+        "• Adquirí habilidades básicas de programación front-end y desarrollo de aplicaciones móviles desde mi formación técnica a temprana edad.",
+      ],
+    },
+  ],
+
+  habilidades: [
+    {
+      name: "Illustrator",
+      icon: "/assets/images/svg/illustrator.svg"
+    },
+    {
+      name: "Angular",
+      icon: "/assets/images/svg/angular.svg"
+    },
+    {
+      name: "Astro",
+      icon: "/assets/images/svg/astro.svg"
+    },
+    {
+      name: "Bootstrap",
+      icon: "/assets/images/svg/bootstrap.svg"
+    },
+    {
+      name: "C",
+      icon: "/assets/images/svg/c.svg"
+    },
+    {
+      name: "C++",
+      icon: "/assets/images/svg/cpp.svg"
+    },
+    {
+      name: "Tailwind",
+      icon: "/assets/images/svg/tailwind.svg"
+    },
+    {
+      name: "HTML",
+      icon: "/assets/images/svg/html.svg"
+    },
+    {
+      name: "Java",
+      icon: "/assets/images/svg/java.svg"
+    },
+    {
+      name: "Javascript",
+      icon: "/assets/images/svg/javascript.svg"
+    },
+    {
+      name: "Kotlin",
+      icon: "/assets/images/svg/kotlin.svg"
+    },
+    {
+      name: "MongoDB",
+      icon: "/assets/images/svg/mongodb.svg"
+    },
+    {
+      name: "MySQL",
+      icon: "/assets/images/svg/mysql.svg"
+    },
+    {
+      name: "NodeJS",
+      icon: "/assets/images/svg/nodejs.svg"
+    },
+    {
+      name: "PHP",
+      icon: "/assets/images/svg/php.svg"
+    },
+    {
+      name: "Python",
+      icon: "/assets/images/svg/python.svg"
+    },
+    
+  ],
+
+  socialMedia: {
+    facebook: "https://www.facebook.com/eddiegmo",
+    github: "https://github.com/marmolejox",
+    email: "mailto:marmolejx@gmail.com",
+    linkedin: "https://www.linkedin.com/in/marmolejx/",
+  },
+
+  projects: [
+    {
+      title: "Odontoclinic",
+      isFeatured: true,
+      thumbnail: "/assets/images/Odontoclinic.webp",
+      githubUrl: "https://github.com/marmolejox/odontoclinic",
+      liveUrl: "https://odontoclinic-ags.vercel.app/",
+    },
+    {
+      title: "Rediseño - Seduzac",
+      isFeatured: false,
+      thumbnail: "/assets/images/Seduzac.webp",
+      githubUrl: "https://github.com/marmolejox/seduzac",
+    },
+    {
+      title: "Academy",
+      isFeatured: false,
+      thumbnail: "/assets/images/fazeacademy.webp",
+      githubUrl: "https://github.com/marmolejox/timecapsule",
+    },
+    {
+      title: "ML Scraper",
+      isFeatured: true,
+      thumbnail: "/assets/images/mlscraper.webp",
+      githubUrl: "https://github.com/marmolejox/mlscraper",
+    },
+  ],
+};
